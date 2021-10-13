1e4c640fdd10374778076893498b413c54ad5f29
JamesSinghal <22singhalj@smtexas.org>
2021-10-13 12:06:18 -0500

    adding code

--- Crystal.js
+++ Crystal.js
@@ -1,5 +1,10 @@
-class Crystal { //Actually draws the crystals by relying on the layers class 
+// Define the Crystal class for use later; the class allows us to easily reuse code and varibles for the many different crystals drawn
+
+class Crystal { 
+  // Constructor is what is called when an instance of the class is created with new Crystal(); in this case it would be new Crystal(posX, posY)
   constructor(posX, posY) {
+    // Read the variables from the constructor into local variables for the class. 
+    // This allows variables of the same name to be used for different instances of the same class
     this.x = posX
     this.y = posY
     this.layers = []
