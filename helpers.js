306038d35fd86920ce077d3abf1ddc78faa34e38
JamesSinghal <22singhalj@smtexas.org>
2021-10-13 12:57:28 -0500

    finalizing code 3

--- helpers.js
+++ helpers.js
@@ -10,7 +10,7 @@ modify it under the terms of the GNU General Public License v3
  as published by the Free Software Foundation
 */
 
-const hexagon = (xPosition, yPosition, radius) => {           //Forms hexagons           
+function hexagon(xPosition, yPosition, radius) {
   const rotAngle = 360 / 6
   beginShape()
   for (let i = 0; i < 6; i++) {
@@ -20,23 +20,23 @@ const hexagon = (xPosition, yPosition, radius) => {           //Forms hexagons
   endShape(CLOSE)
 }
 
-const pointOnCircle = (xPosition, yPosition, radius, angle) => { // Forms the Poins and repating points in the snowflake  
+function pointOnCircle(xPosition, yPosition, radius, angle) {
   const x = xPosition + radius * cos(angle)
   const y = yPosition + radius * sin(angle)
   return createVector(x, y)
 }
 
-const randomSelectTwo = () => {
+function randomSelectTwo() {
   const rando = random(1)
   return rando > 0.5 ? true : false
 }
 
-const getRandomFromCOLOR_ARRAY = () => {
+function getRandomFromCOLOR_ARRAY() {
   const rando = floor(random(0, COLOR_ARRAY.length))
   return COLOR_ARRAY[rando]
 }
 
-const testLines = (state) => {
+function testLines(state) {
   state.numShapes = randomSelectTwo() ? state.SECTIONS : state.SECTIONS * 2
   state.angle = 360 / state.numShapes
 
@@ -50,32 +50,32 @@ const testLines = (state) => {
       push()
       // translate(width / 2, height / 2) //**
       if (state.lines) {
-        for (let i = 0; i < 360 - 0.1; i += state.angle) { 
-          line(0, 0, 0, CRYSTALWIDTH / 2)                  
+        for (let i = 0; i < 360 - 0.1; i += state.angle) {
+          line(0, 0, 0, CRYSTALWIDTH / 2)
           rotate(state.angle)
         }
       }
       if (state.circle) {
-        ellipse(0, 0, CRYSTALWIDTH, CRYSTALWIDTH)            
+        ellipse(0, 0, CRYSTALWIDTH, CRYSTALWIDTH)
       }
       pop()
     }
   })
 }
 
-const myTriangle = (center, radius, direction) => {
+function myTriangle(center, radius, direction) {
   if (direction) {
-    beginShape();
-    vertex(center + radius * cos(0), radius * sin(0));
-    vertex(center + radius * cos(120), radius * sin(120));
-    vertex(center + radius * cos(240), radius * sin(240));
-    endShape(CLOSE); 
+    beginShape()
+    vertex(center + radius * cos(0), radius * sin(0))
+    vertex(center + radius * cos(120), radius * sin(120))
+    vertex(center + radius * cos(240), radius * sin(240))
+    endShape(CLOSE)
   } else {
-    beginShape();
-    vertex(center + radius * cos(180), radius * sin(180));
-    vertex(center + radius * cos(300), radius * sin(300));
-    vertex(center + radius * cos(60), radius * sin(60));
-    endShape(CLOSE);
+    beginShape()
+    vertex(center + radius * cos(180), radius * sin(180))
+    vertex(center + radius * cos(300), radius * sin(300))
+    vertex(center + radius * cos(60), radius * sin(60))
+    endShape(CLOSE)
   }
 }
 
